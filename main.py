bbc3d02312cb49e4b26ed78cc0718fb6c871f78f
Lucas <lucassmontagnee@gmail.com>
2021-12-13 22:05:11 +0100

    Update - show map

--- main.py
+++ main.py
@@ -17,27 +17,30 @@ class WeatherWidget(QWidget):
         super().__init__(parent=parent)
 
         # Main layout
-        layout = QVBoxLayout()
+        self.layout = QVBoxLayout()
 
         if self.init:
-            self.lineEditLat = 0
-            self.lineEditLon = 0
+            lat = QLineEdit("43.5571085")
+            lon = QLineEdit("1.4684552")
+            lat = float(lat.text())
+            lon = float(lon.text())
             self.init = 0
 
-        self.m = folium.Map(location=[self.lineEditLat, self.lineEditLon], zoom_start=13)
+        self.m = folium.Map(location=[lat, lon], zoom_start=13)
         self.data = io.BytesIO()
+
         self.m.save(self.data, close_file=False)
         self.carte = QtWebEngineWidgets.QWebEngineView()
         self.carte.setHtml(self.data.getvalue().decode())
-        layout.addWidget(self.carte)
+        self.layout.addWidget(self.carte)
 
         # Text Zone
         self.lineEditLat = QLineEdit("43.5571085")
         self.lineEditLon = QLineEdit("1.4684552")
-        layout.addWidget(QLabel("Latitude"))
-        layout.addWidget(self.lineEditLat)
-        layout.addWidget(QLabel("Longitude"))
-        layout.addWidget(self.lineEditLon)
+        self.layout.addWidget(QLabel("Latitude"))
+        self.layout.addWidget(self.lineEditLat)
+        self.layout.addWidget(QLabel("Longitude"))
+        self.layout.addWidget(self.lineEditLon)
 
         # Ville
         self.labelIconCity = QLabel(self)
@@ -66,17 +69,27 @@ class WeatherWidget(QWidget):
         # Validation button
         self.validateButton = QPushButton("Valider")
         self.validateButton.clicked.connect(self.getWeatherInfos)
-        layout.addWidget(self.validateButton)
+        self.layout.addWidget(self.validateButton)
 
         # Display infos
-        layout.addLayout(hLayoutTemp)
-        layout.addLayout(hLayoutVille)
-        self.setLayout(layout)
+        self.layout.addLayout(hLayoutTemp)
+        self.layout.addLayout(hLayoutVille)
+        self.setLayout(self.layout)
 
     def getWeatherInfos(self):
         weatherDict = getWeatherInfos(float(self.lineEditLat.text()), float(self.lineEditLon.text()))
 
+        lat = self.lineEditLat
+        lon = self.lineEditLon
+        lat = float(lat.text())
+        lon = float(lon.text())
+        print(lat, lon)
+        self.m = folium.Map(location=[lat, lon], zoom_start=13)
+        self.data = io.BytesIO()
         self.m.save(self.data, close_file=False)
+        self.carte = QtWebEngineWidgets.QWebEngineView()
+        self.carte.setHtml(self.data.getvalue().decode())
+        
 
         self.labelCity.setText(weatherDict['name'])
         self.labelTemp.setText(str(round(weatherDict['main']['temp'] - 273.15, 2)) + " °C")
